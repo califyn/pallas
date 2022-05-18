35c9c8c3bf204522fadd906986c95f202cc94098
Ali Cy <califynic@gmail.com>
2022-05-18 03:04:02 +0000

    React demo complete
    Available at https://pallas.athemath.org, with all necessary
    changes. Run with npm run react-start. Refers to correct certs.
    Runs on correct ports. Uses a tiny tiny bit of React.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-document.write('Hello World!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  const style = {
+    padding: '40px',
+    textAlign: 'center',
+  }
+
+  return <div style={style}>Welcome to React!</div>
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = env => {
   return {
     mode: env.development ? 'development' : 'production',
     entry: './index.js',
     output: {
       filename: 'bundle.js',
+    },
+    devServer: {
+      host: 'pallas.athemath.org',
+      port: '443',
+      server: {
+        type: 'https',
+        options: {
+          key: fs.readFileSync('./certs/privkey.pem'),
+          cert: fs.readFileSync('./certs/fullchain.pem')
+        }
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+              },
+            },
+          ],
+        },
+      ],
     },
   }
 }
